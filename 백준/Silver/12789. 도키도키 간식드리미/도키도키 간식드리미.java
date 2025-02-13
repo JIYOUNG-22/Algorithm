27d8f187c9ce7c8667e0c0710343637cb53420eb
JIYOUNG-22 <dyong0922@naver.com>
2025-02-13 09:30:32 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 124 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());	// 학생 수 n
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		Stack<Integer> stack = new Stack<>();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			queue.offer(Integer.parseInt(st.nextToken()));
+		}
+		
+		int number = 1;
+		while(number <= n) {
+			if(queue.contains(number)) {
+				while(queue.peek() != number) {
+					stack.push(queue.poll());
+				}
+				queue.poll();
+			} else if(stack.contains(number)) {
+				if(stack.peek() == number) stack.pop();
+				else break;
+			}
+			number++;
+		}
+		
+		System.out.println(number >= n ? "Nice" : "Sad");
+	}
+}
