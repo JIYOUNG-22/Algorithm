4762e07fcc5528ebe162f0c46ea88b4e608b6d7e
JIYOUNG-22 <dyong0922@naver.com>
2025-02-16 21:40:47 +0900

    [Silver III] Title: 죽음의 게임, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int n, k;
+    static int[] point;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());	// 게임 참여자 수
+        k = Integer.parseInt(st.nextToken());	// 보성이의 번호 (index)
+
+        point = new int[n];
+        for(int i = 0; i < n; i++) {
+            point[i] = Integer.parseInt(br.readLine());
+        }
+
+        int dead = 0, count = 0;
+        Set<Integer> set = new HashSet<>();
+        while(dead != k) {
+            dead = point[dead];
+            count++;
+
+            if(set.contains(dead)) {
+                count = -1;
+                break;
+            }
+            set.add(dead);
+        }
+
+        System.out.println(count);
+    }
+}
