2d543b6d84d1be1bfdd63359a3758d5fd28fa0da
JIYOUNG-22 <dyong0922@naver.com>
2025-02-20 22:46:51 +0900

    [Gold V] Title: 탑, Time: 680 ms, Memory: 85212 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());   // 탑의 수 N
+
+        int[] towers = new int[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i < N + 1; i++) {
+            towers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[N+1];
+        for(int i = N; i > 0; i--) {
+            if(stack.isEmpty()) stack.push(i);
+
+            while(!stack.isEmpty() && towers[stack.peek()] < towers[i]) {
+                result[stack.pop()] = i;
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()) {
+            result[stack.pop()] = 0;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < N + 1; i++) {
+            sb.append(result[i]).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
+}
