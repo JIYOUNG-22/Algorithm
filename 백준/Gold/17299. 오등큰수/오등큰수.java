24419734076017a5e0a4813ac0c49695c861d402
JIYOUNG-22 <dyong0922@naver.com>
2025-02-20 10:45:26 +0900

    [Gold III] Title: 오등큰수, Time: 1360 ms, Memory: 225460 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -1,57 +1,45 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());   // 수열 A의 크기 (1 <= n <= 1,000,000)
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] input = new int[n];
-        int[] arrayF = new int[n];
-        int temp;
-
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
-            temp = Integer.parseInt(st.nextToken());
-            input[i] = temp;
-
-            if(map.containsKey(temp)) {
-                map.put(temp, map.get(temp) + 1);
-            } else {
-                map.put(temp, 1);
-            }
-        }
-
-        for(int i = 0; i < n; i++) {
-            arrayF[i] = map.get(input[i]);
-        }
-
-        Stack<Integer> stack = new Stack<>();
-        int[] result = new int[n];
-
-        for(int i = 0; i < n; i++) {
-            result[i] = -1;
-        }
-
-        for(int i = 0; i < n; i++) {
-            while(!stack.isEmpty() && arrayF[stack.peek()] < arrayF[i]) {
-                result[stack.pop()] = input[i];
-            }
-            stack.push(i);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int ngf : result) {
-            sb.append(ngf).append(" ");
-        }
-        System.out.println(sb.toString().trim());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());   // 수열 A의 크기
+		
+		int[] originArr = new int[n];
+		Map<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			originArr[i] = num;
+			map.put(num, map.getOrDefault(num, 0) + 1);
+		}
+		
+		int[] countArr = new int[n];
+		for(int i = 0; i < n; i++) {
+			countArr[i] = map.get(originArr[i]);
+		}
+		
+		Stack<Integer> stack = new Stack<>();
+		int[] result = new int[n];
+		for(int i = 0; i < n; i++) {
+			if(stack.isEmpty()) stack.push(i);
+			
+			while(!stack.isEmpty() && countArr[stack.peek()] < countArr[i]) {
+				result[stack.pop()] = originArr[i];
+			}
+			
+			stack.push(i);
+		}
+		
+		while(!stack.isEmpty()) 
+			result[stack.pop()] = -1;
+		
+	 StringBuilder sb = new StringBuilder();
+      for(int i : result) {
+          sb.append(i).append(" ");
+      }
+      
+    System.out.println(sb.toString().trim());
+	}
 }
