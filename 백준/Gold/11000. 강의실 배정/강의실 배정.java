08ab5c405e363dba9bec08435ad3fba23bf12ef3
JIYOUNG-22 <dyong0922@naver.com>
2025-02-26 08:30:01 +0900

    [Gold V] Title: 강의실 배정, Time: 608 ms, Memory: 72208 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -2,49 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+	static class Lecture implements Comparable<Lecture> {
+		int start;
+		int end;
+		
+		public Lecture(int s, int t) {
+			this.start = s;
+			this.end = t;
+		}
+		
+		@Override
+		public int compareTo(Lecture l) {
+			return this.start - l.start;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());   // 강의 개수 N
 		
-		List<Map<Integer, Integer>> classes = new ArrayList<>();
+		List<Lecture> lectures = new ArrayList<>();
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			
-			Map<Integer, Integer> classInfo = new HashMap<>();
-      classInfo.put(start, end);
-      classes.add(classInfo);
+			lectures.add(new Lecture(start, end));
 		}
 		
-		Collections.sort(classes, new Comparator<Map<Integer, Integer>>() {
-			@Override
-			public int compare(Map<Integer, Integer> o1, Map<Integer, Integer> o2) {
-				return o1.keySet().iterator().next().compareTo(o2.keySet().iterator().next());
-			}
-		});
+		Collections.sort(lectures);
 		
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		int result = 1;
-		for(Map<Integer, Integer> classInfo : classes) {
-			Map.Entry<Integer, Integer> entry = classInfo.entrySet().iterator().next();
-			int start = entry.getKey();
-			int end = entry.getValue();
 		
+		for(Lecture l : lectures) {
 			if(pq.isEmpty()) {
-				pq.add(end);
+				pq.add(l.end);
 				continue;
 			}
 			
-			if(start >= pq.peek()) {
+			if(l.start >= pq.peek()) {
 				pq.poll();
-				pq.offer(end);
+				pq.offer(l.end);
 			} else {
-				pq.offer(end);
-				result++;
+				pq.offer(l.end);
 			}
 		}
 		
-		System.out.println(result);
+		System.out.println(pq.size());
 	}
 }
