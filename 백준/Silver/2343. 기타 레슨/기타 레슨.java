e58e749e858445df2b221528311cf4ee8354ca8e
JIYOUNG-22 <dyong0922@naver.com>
2025-03-26 09:41:51 +0900

    [Silver I] Title: 기타 레슨, Time: 288 ms, Memory: 23884 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,38 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-public static int[] A;
-public static int[] videoSize;
-public static int sum;
-public static int count;
-public static int N;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		int[] A = new int[N];
-		int start = 0;
-		int end =0;
-		for(int i=0; i<N; i++) {
-			A[i] = sc.nextInt();
-			if(start < A[i]) start = A[i];
-			end = end + A[i];
-		}
-		while(start <= end) {
-			int middle = (start + end)/2;
-			int sum =0;
-			int count =0;
-			for(int i=0; i<N; i++) {
-				if(sum + A[i] > middle) {
-					count++;
-					sum =0;
-				}
-				sum = sum + A[i];
-			}
-			if(sum != 0) count++;
-			if(count > M) start = middle +1;
-			else end = middle -1;
-		}
-		System.out.println(start);
-	}
+
+    static int n, m;
+    static int[] lectures;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        lectures = new int[n];
+        st = new StringTokenizer(br.readLine());
+        int min = 0;
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            lectures[i] = Integer.parseInt(st.nextToken());
+            if(min < lectures[i]) min = lectures[i];
+            max += lectures[i];
+        }
+
+        System.out.println(blu_ray(min, max));
+
+    }
+
+    static int blu_ray(int low, int high) {
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            int cnt = 0, sum = 0;
+            for(int i = 0; i < n; i++) {
+                if(sum + lectures[i] > mid) {
+                    cnt++;
+                    sum = 0;
+                }
+                sum += lectures[i];
+            }
+            if(sum != 0) cnt++;
+            if(cnt > m) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low;
+    }
 }
